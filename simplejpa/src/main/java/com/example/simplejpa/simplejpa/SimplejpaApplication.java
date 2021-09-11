1d562353c1b09f70de5618db916671bb6bca0a60
emilyjspencer <emilyspencer89@hotmail.co.uk>
2021-09-11 15:04:44 +0100

    Generates a spring boot project

---
+++ simplejpa/src/main/java/com/example/simplejpa/simplejpa/SimplejpaApplication.java
@@ -0,0 +1,13 @@
+package com.example.simplejpa.simplejpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimplejpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimplejpaApplication.class, args);
+	}
+
+}
---
+++ simplejpa/src/test/java/com/example/simplejpa/simplejpa/SimplejpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.simplejpa.simplejpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimplejpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
